3ac0ba07f1737f74f883e80daa8e7f2deba9ae4a
Susant-Basnet <be2018se629@gces.edu.np>
2024-02-02 13:07:51 +0545

    wip

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 
-const cell = document.querySelector('.cell')
+let currentMarker = "X";
+let gameOver = false;
 
-function startGame(){
-  
+const winningConditions = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+];
+
+
+
+
+
+function restartGame() {
+    
+    
 }
